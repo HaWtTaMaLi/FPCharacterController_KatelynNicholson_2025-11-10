60311cc1fc2c8315622bece68d00b01b6fcafbd4
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-10 13:15:38 -0400

    push

--- Assets/Character/CameraController.cs
+++ Assets/Character/CameraController.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float xRotation = 0f;
+    public float mouseSensitivity = 100f;
+
+    //Player Refference
+    public Transform playerBody;
 
     void Start()
     {
-        
+        //hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity + Time.deltaTime;
+
+        xRotation -= mouseY;
+        //clamp
+        xRotation = Mathf.Clamp(xRotation, -90f, 90); //min and max look
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
+
--- Assets/Character/PlayerController.cs
+++ Assets/Character/PlayerController.cs
@@ -2,43 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    //reference camera
-
-    //steps are .5 height
-    //jump is 1
-    //Ramps 
-    //wasd
-
-    //Look Direction
-    //public float mouseX;
-    //public float mouseY;
-    public float xRotation = 0f;
-    public float mouseSensitivity = 100f;
-
-
-    //Player
-    public Transform playerBody;
-
-    //Actions
-    public int speed = 5;
-
-    void Start()
+    private void Start()
     {
-        //hide cursor
-        Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
-    void Update()
+    private void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity + Time.deltaTime;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        xRotation -= mouseY;
-        //clamp
-        xRotation = Mathf.Clamp(xRotation, -90f, 90); //min and max look
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        Vector3 move = new Vector3(x, 0f, z);
     }
+
 }
