73808fdba35afe86f93c41bfb7470371eb8a99c0
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-12 16:32:32 -0400

    push

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Controls")]
+    public int speed = 5;
+    public int jumpHight = 1;
+    public float gravity = -9.8f;
+
+    public CharacterController controller;
+    public Vector2 moveInput;
+    public Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext input)
+    {
+        moveInput = input.ReadValue<Vector2>();
+    }
+
+    public void OnJump()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
