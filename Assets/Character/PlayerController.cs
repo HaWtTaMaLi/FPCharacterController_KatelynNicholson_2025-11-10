d2113c21871dfdb49f74617c32cd509f5ecfde05
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-17 12:05:32 -0400

    push after conflict

--- Assets/Character/PlayerController.cs
+++ Assets/Character/PlayerController.cs
@@ -3,5 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject Player;
+
+    public int speed = 5;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9f;
+
+    public CharacterController playerController; //get component
+    public bool isGrounded = true;
+    public Vector3 playerVelocity;
+
+    [Header("Input Actions")]
+    public InputActionReference Move; //vector2
+    public InputActionReference Look; //vector2
+    public InputActionReference Jump; //button
+    public InputActionReference Sprint; //button
+    public InputActionReference Interact; //button
+    public InputActionReference Crouch; //button
+
+    public void Update()
+    {
+        isGrounded = playerController.isGrounded;
+
+        float x = Input.GetAxis("Horizontal"); //W && S
+        float z = Input.GetAxis("Vertical"); //A && D
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Player.transform.position += move * speed * Time.deltaTime; //change transform to Move character controller methods
+
+    }
 
 }
--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,20 +2,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //[Header("Look Sensitivity")]
-   // public float mouseSensitivity = 100f;
-    //public Transform player;
+    public float xRotation = 0f;
+    public float mouseSensitivity = 100f;
+
+    //Player Refference
+    public Transform playerBody;
 
     void Start()
     {
-        
+        //hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        //float mouseY = Input.GetAxis("Move Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //player.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 30); //min and max look
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
+
